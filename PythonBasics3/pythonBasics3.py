a5e2b0c77c207e746f2c994b827a10a871f9bab7
David Brown <dbrow206@uncc.edu>
2020-10-13 13:37:51 -0400

    PythonBasics 3 - added logic to starts_with_number function

--- PythonBasics3/pythonBasics3.py
+++ PythonBasics3/pythonBasics3.py
@@ -12,9 +12,11 @@ import re
 # if it starts with a number and false otherwise.
 # (For our purposes, a number is any character that is 0,1,2,3,4,5,6,7,8, or 9.)
 def starts_with_number(s):
-  # YOUR CODE HERE
 
-  return
+  if len(s) == 0:
+    return False
+
+  return s[0].isdigit()
 
 # # Part B. starts_with_consonant
 # Define a function starts_with_consonant(s) that takes a string and returns true
