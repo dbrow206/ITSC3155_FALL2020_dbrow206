48a68a675fe004379486b05e8db838f20f4e8221
David Brown <dbrow206@uncc.edu>
2020-10-29 12:59:16 -0400

    Pythonbasic4 - Part A

--- PythonBasics4/pythonBasics4.py
+++ PythonBasics4/pythonBasics4.py
@@ -6,19 +6,46 @@
 
 # # Part A.
 def array_2_dict(emails, contacts):
-    # YOUR CODE HERE
+    counter = 0
+    for i in emails:
+        contacts[list(contacts.keys())[counter]] = i
+        counter = counter + 1
+        return contacts
 
     return
+
 
 # # Part B.
 def array2d_2_dict(contact_info, contacts):
-    # YOUR CODE HERE
+    #
+    if (len(contact_info) == 0):
+        return contacts
+    counter = 0
+
+    for i in contact_info:
+        if (len(i) == 0):
+            return contacts
+        contact = {"email": i[0], "phone": i[1]}
+    contacts[list(contacts.keys())[counter]] = contact
+    counter = counter + 1
+    return contacts
 
     return
+
 
 # # Part C.
 def dict_2_array(contacts):
-    # YOUR CODE HERE
+    nameArray=[]
+    emailArray=[]
+    phoneArray=[]
 
+    if(len(list(contacts.keys())) ==0):
+        return [[],[],[]]
+
+    for i in list(contacts.keys()):
+        nameArray.append(i)
+        emailArray.append(contacts[i]['email'])
+        phoneArray.append(contacts[i]['phone'])
+
+    return[emailArray,phoneArray,nameArray]
     return
-
